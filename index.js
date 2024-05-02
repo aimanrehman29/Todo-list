ff992d9e1407fd6ff789a21e3337b76282dd5e98
Ayman Rehman <aimanshahzad295@gmail.com>
2024-05-02 05:57:51 +0500

    it is a todo list type script code

---
+++ index.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    let addwork = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "what  you want  to do ",
+        },
+        {
+            name: "todomore",
+            type: "confirm",
+            message: "want do more works",
+            default: "false",
+        },
+    ]);
+    let { todo, todomore } = addwork;
+    console.log(addwork);
+    loop = todomore;
+    if (todo) {
+        todos.push(addwork.todo);
+    }
+    else {
+        console.log("kindly add sometheing");
+    }
+    if (todos.length > 0) {
+        console.log("here your to do list");
+        todos.forEach((todo) => {
+            console.log(todo);
+        });
+    }
+    else {
+        console.log("no todos found ");
+        ``;
+    }
+}
